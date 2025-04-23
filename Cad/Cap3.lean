a0a6e69099b912d1b5a53c88d63b0a5fd923dfa3
Tomaz <tomgm1502@gmail.com>
2025-04-23 13:50:11 -0300

    Add definition of semialgebraic function

--- Cad/Cap3.lean
+++ Cad/Cap3.lean
@@ -1,20 +1,20 @@
 import Mathlib
 
-open Classical
-
 namespace SemiAlgebraic
 
-def R (k : Nat) : Type := (Fin k → ℝ)
+open Classical
 
-def Roots (P: MvPolynomial (Fin k) ℝ) : Set (R k) := fun f => P.eval f = 0
+def R (k : Nat) : Type := Fin k → ℝ
 
-def Pos (Qs : Finset (MvPolynomial (Fin k) ℝ)) : Set (R k) :=
+def Roots {k : Nat} (P: MvPolynomial (Fin k) ℝ) : Set (R k) := fun f => P.eval f = 0
+
+def Pos {k : Nat} (Qs : Finset (MvPolynomial (Fin k) ℝ)) : Set (R k) :=
   fun f => ∀ Q ∈ Qs , Q.eval f > 0
 
 inductive BasicSA (k : Nat) where
   | mk : MvPolynomial (Fin k) ℝ → Finset (MvPolynomial (Fin k) ℝ) → BasicSA k
 
-def interpBasicSA' (b : BasicSA k) : Set (R k) :=
+def interpBasicSA {k : Nat} (b : BasicSA k) : Set (R k) :=
   match b with
   | .mk P Qs =>
     let rP := Roots P
@@ -22,23 +22,25 @@ def interpBasicSA' (b : BasicSA k) : Set (R k) :=
     rP ∩ pQ
 
 -- Semialgebraic sets
-def SA (b : Finset (BasicSA k)) : Set (R k) :=
-  let bs : Finset (Set (R k)) := b.image interpBasicSA'
+def SA {k : Nat} (b : Finset (BasicSA k)) : Set (R k) :=
+  let bs : Finset (Set (R k)) := b.image interpBasicSA
   Finset.fold (fun x1 x2 => x1 ∪ x2) {} id bs
 
-def proj {k : Nat} (a : Set (Fin (k + 1) → ℝ)) : Set (Fin k → ℝ) :=
+def proj {k : Nat} (a : Set (R (k + 1))) : Set (R k) :=
   a.image (fun z: (Fin (k + 1) → ℝ) => (fun x: Fin k => z x))
 
-def IsSemiAlgebraic (s : Set (R k)) : Prop := ∃ (b : Finset (BasicSA k)) , SA b = s
+def IsSemiAlgebraic {k : Nat} (s : Set (R k)) : Prop := ∃ (b : Finset (BasicSA k)) , SA b = s
 
 def relToSet {a b : Type} : Rel a b → Set (a × b) := fun R => { r: a × b | R r.1 r.2 }
 
-def conjSet {n m : Nat} : Set (R n × R m) → Set (R (n + m)) := fun s: Set (R n × R m) =>
-  fun x: R (n + m) => sorry
+def castProd {n m : Nat} : R n × R m → R (n + m) :=
+  fun ⟨r₁, r₂⟩ => fun r => if h: r < n then r₁ ⟨r, h⟩ else r₂ ⟨r - n, by omega⟩
+
+def castSetProd {n m : Nat} : Set (R n × R m) → Set (R (n + m)) := fun s: Set (R n × R m) =>
+  s.image castProd
 
 def IsSemiAlgebraicFunction (n m : Nat) (f : R n -> R m) : Prop :=
-  let s := relToSet (f.graph)
-  let s' := conjSet s
-  IsSemiAlgebraic s'
+  let s: Set (R n × R m) := relToSet f.graph
+  IsSemiAlgebraic (castSetProd s)
 
 end SemiAlgebraic
