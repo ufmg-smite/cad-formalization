118b6f29bd458e4fb86d53c546f1428a21a530fa
Tomaz Gomes Mascarenhas <tomgm1502@gmail.com>
2025-08-27 19:54:53 -0300

    Everything is noncomputable

--- Cad/SturmBasu.lean
+++ Cad/SturmBasu.lean
@@ -5,7 +5,9 @@ open Set
 open Filter
 open Classical
 
-noncomputable def polyRemSeq (f g : Polynomial ℝ) (h : g.natDegree ≠ 0) : List (Polynomial ℝ) :=
+noncomputable section
+
+def polyRemSeq (f g : Polynomial ℝ) (h : g.natDegree ≠ 0) : List (Polynomial ℝ) :=
   go f g [f] h
 where
   go (h₀ h₁ : Polynomial ℝ) (acc : List (Polynomial ℝ)) (j : h₁.natDegree ≠ 0): List (Polynomial ℝ) :=
@@ -20,7 +22,7 @@ where
     apply Polynomial.natDegree_mod_lt
     exact j
 
-noncomputable def sturmSeq (f g : Polynomial ℝ) : List (Polynomial ℝ) :=
+def sturmSeq (f g : Polynomial ℝ) : List (Polynomial ℝ) :=
   if h : g.natDegree = 0 then
     [f, g]
   else
@@ -32,7 +34,7 @@ noncomputable def sturmSeq (f g : Polynomial ℝ) : List (Polynomial ℝ) :=
       aux ++ [gcdf]
 
 -- Considerando só os não nulos
-noncomputable def seqVar : List ℝ → ℕ
+def seqVar : List ℝ → ℕ
 | [] => 0
 | _::[] => 0
 | a::(b::as) =>
@@ -43,23 +45,23 @@ noncomputable def seqVar : List ℝ → ℕ
   else
     seqVar (b::as)
 
-noncomputable def seqEval (k : ℝ) : List (Polynomial ℝ) → List ℝ
+def seqEval (k : ℝ) : List (Polynomial ℝ) → List ℝ
 | [] => []
 | a::as => [eval k a]++(seqEval k as)
 
-noncomputable def sgn (k : ℝ) : ℤ  :=
+def sgn (k : ℝ) : ℤ  :=
   if k > 0 then 1
   else if k = 0 then 0
   else -1
 
-noncomputable def rootsInInterval (f : Polynomial ℝ) (a b : ℝ) : Finset ℝ :=
+def rootsInInterval (f : Polynomial ℝ) (a b : ℝ) : Finset ℝ :=
   f.roots.toFinset.filter (fun x => x ∈ Ioo a b)
 
-noncomputable def c_pos (f g : Polynomial ℝ) (a b : ℝ) : ℕ :=
+def c_pos (f g : Polynomial ℝ) (a b : ℝ) : ℕ :=
   let k : Finset ℝ := rootsInInterval f a b
   (k.filter (fun x => eval x g > 0)).card
 
-noncomputable def c_neg (f g : Polynomial ℝ) (a b : ℝ) : ℕ :=
+def c_neg (f g : Polynomial ℝ) (a b : ℝ) : ℕ :=
   let k : Finset ℝ := rootsInInterval f a b
   (k.filter (fun x => eval x g < 0)).card
 
@@ -71,7 +73,7 @@ def sign_r_pos (x : ℝ) (p : Polynomial ℝ) : Prop :=
 
 -- 1 if p / q goes from -inf to +inf in x, -1 if goes from +inf to -inf
 -- 0 otherwise
-noncomputable def jump_val (p q : Polynomial ℝ) (x : ℝ) : ℤ :=
+def jump_val (p q : Polynomial ℝ) (x : ℝ) : ℤ :=
   let orderP : Int := rootMultiplicity x p
   let orderQ : Int := rootMultiplicity x q
   let oddOrder := Odd (orderP - orderQ)
@@ -81,7 +83,7 @@ noncomputable def jump_val (p q : Polynomial ℝ) (x : ℝ) : ℤ :=
   else 0
 
 -- Corresponde a Ind(Q/P; a, b)
-noncomputable def cauchyIndex (p q : Polynomial ℝ) (a b : ℝ) : ℤ :=
+def cauchyIndex (p q : Polynomial ℝ) (a b : ℝ) : ℤ :=
   ∑ x ∈ rootsInInterval p a b, jump_val p q x
 
 theorem Sturm (f g : Polynomial ℝ) (a b : ℝ) (h : a < b) :
