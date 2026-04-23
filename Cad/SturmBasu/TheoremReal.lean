f5e0aee76747b02958351659117e84afb7fcc8f7
sarahazp <sarahcomh@proton.me>
2026-04-23 16:48:36 -0300

    more on RealEquiv

--- Cad/SturmBasu/TheoremReal.lean
+++ Cad/SturmBasu/TheoremReal.lean
@@ -15,22 +15,32 @@ lemma toPolyReal_zero (p : CPolynomial Rat) : p ≠ 0 → toPolyReal p ≠ 0 := 
   intros h
   exact Polynomial.map_ne_zero (gneg_imp_gtopoly_neg p h)
 
+lemma toPolyReal_ne_zero : toPolyReal 0 = 0 := by
+  rw[toPolyReal]
+  simp only [CPolynomial.toPoly_zero, Polynomial.map_zero]
+
 theorem equiv_for_Realsturm_tarski_interval (a b : ℚ) (p q : CPolynomial ℚ) :
     seqVarSturm_ab_CPolynomial p (CPolynomial.derivative (p) * q) a b = seqVarSturm_ab (toPolyReal p) ((toPolyReal p).derivative * (toPolyReal q)) a b := by
   rw[seqVarSturm_ab_CPolynomial, seqVar_ab_CPolynomial, seqEval_CPolynomial.eq_def, sturmSeq_CPolynomial]--
   rw[seqVarSturm_ab, sturmSeq, seqVar_ab, seqEval.eq_def]
 
   if h : p = 0 then
-    have : toPolyReal p = 0 := by rw[h]; sorry
+    have : toPolyReal p = 0 := by
+      rw[h];
+      apply toPolyReal_ne_zero
     simp_all
     rw[seqEval_CPolynomial, seqEval]; simp_all
   else
     have : ¬ toPolyReal p = 0 := by
-      sorry
+      apply toPolyReal_zero p h
     simp_all
-    have : CPolynomial.eval a p = (Polynomial.eval (a:Real) (toPolyReal p)) := by
+    let k := (CPolynomial.eval a p)
+    have : (k : Real)= (Polynomial.eval (a:Real) (toPolyReal p)) := by
+      unfold k
+      have := CPolynomial.eval_toPoly a p
       sorry
-    rw[← this]
+    rw[← this]; unfold k
+
     sorry
   --  simp only [derivative_equiv]
   --  simp only [seq_eval_equiv]
