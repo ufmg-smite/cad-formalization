73302bc4cdc82e88d0e9dbb557305fc1ca04b275
Tomaz Gomes Mascarenhas <tomgm1502@gmail.com>
2026-06-19 15:44:28 -0300

    Add rule 4.3

---
+++ Cad/Multivariate/ProofCalculus/Rule4_3.lean
@@ -0,0 +1,21 @@
+import Cad.Multivariate.ProofCalculus.Defs
+
+-- Note that MvPolynomial (Fin 0) ℝ is isomorphic to ℝ:
+/- variable (p : MvPolynomial (Fin 0) ℝ) -/
+
+/- def empty_assign : Fin 0 → ℝ := by -/
+/-   intro abs -/
+/-   cases abs -/
+/-   linarith -/
+
+/- #check MvPolynomial.eval empty_assign p -- ℝ -/
+
+theorem nalbach_4_3_part1 (p : MvPolynomial (Fin 0) ℝ) : sgn_inv 0 Set.univ p := by
+  intros a ha b hb
+  have : a = b := List.ofFn_inj.mp rfl
+  rw [this]
+
+theorem nalbach_4_3_part2 (p : MvPolynomial (Fin 0) ℝ) : ord_inv 0 Set.univ p := by
+  intros a ha b hb
+  have : a = b := List.ofFn_inj.mp rfl
+  rw [this]
